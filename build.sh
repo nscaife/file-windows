c60472d0ff5b967ae24b89fe8dc3a29b4d37b359
Nolen Scaife <nolen@scaife.org>
2017-01-08 11:55:43 -0800

    update build script - fix errors and add output dir

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+mkdir dist/
 cd libgnurx-2.5
 make
 sudo cp regex.h /usr/x86_64-w64-mingw32/include/
 sudo cp libregex.a /usr/x86_64-w64-mingw32/lib/
 sudo cp libgnurx.dll.a /usr/x86_64-w64-mingw32/lib/
+cp libgnurx-0.dll ../dist/
 cd ../file/
 autoreconf -f -i
+./configure --disable-silent-rules --enable-fsect-man5
+make -j4
+cp magic/magic.mgc ../dist/
+make clean
 ./configure --disable-silent-rules --enable-fsect-man5 --host=x86_64-w64-mingw32
 make -j4
+cp src/.libs/libmagic-1.dll ../dist/
+cp src/.libs/file.exe ../dist/

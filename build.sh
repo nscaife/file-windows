37ec7d2e70158a576127fec92e4f60290c33c59f
Yaroslav Halchenko <debian@onerussian.com>
2020-07-13 11:17:21 -0400

    Specify location to libgnurx via env vars not via sudo copy

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+set -eu
+export PS4='> '
+set -x
 mkdir dist/
 cd libgnurx-2.5
 make
-sudo cp regex.h /usr/x86_64-w64-mingw32/include/
-sudo cp libregex.a /usr/x86_64-w64-mingw32/lib/
-sudo cp libgnurx.dll.a /usr/x86_64-w64-mingw32/lib/
+#sudo cp regex.h /usr/x86_64-w64-mingw32/include/
+#sudo cp libregex.a /usr/x86_64-w64-mingw32/lib/
+#sudo cp libgnurx.dll.a /usr/x86_64-w64-mingw32/lib/
+export LDFLAGS="-L$PWD"
+export CFLAGS="-I$PWD"
 cp COPYING.LIB ../dist/COPYING.libgnurx
 cp libgnurx-0.dll ../dist/
 cd ../file/
